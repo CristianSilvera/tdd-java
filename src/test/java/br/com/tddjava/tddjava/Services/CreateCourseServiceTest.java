9dcf1357388c9fbcd2219f9e3758c3e8c4f2eff9
Cristian Silvera <cristiansilveraa@gmail.com>
2023-08-15 23:12:10 -0300

    doc: tdd java project

---
+++ src/test/java/br/com/tddjava/tddjava/Services/CreateCourseServiceTest.java
@@ -0,0 +1,15 @@
+package br.com.tddjava.tddjava.Services;
+
+public class CreateCourseServiceTest {
+    public void should_be_able_to_create_a_new_course () {
+//        Crear un nuevo curso
+
+//        Crear un nuevo servicio
+
+//        Crear tabla curso (Entidad)
+
+//        Crear un repositorio de curso
+
+    }
+
+}
